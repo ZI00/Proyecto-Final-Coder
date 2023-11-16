fb2895958668971b4b083fc7e6c8af04e893365b
Lautaro Reinaga <lautarotreinaga@gmail.com>
2023-11-16 13:07:29 -0300

    segunda view

--- sistema_coder/urls.py
+++ sistema_coder/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from sistema_coder.views import saludar
+from sistema_coder.views import saludar, saludar_con_fecha
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #aca van mis urls
     #path("dreccion", view)
     path("saludo/", saludar),
+    path("saludar-fecha/", saludar_con_fecha),
 ]
--- sistema_coder/views.py
+++ sistema_coder/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from datetime import datetime
+
 def saludar(request):
     saludo = "ghola usuario/a"
+    respuesta_http = HttpResponse(saludo)
+    return respuesta_http
+
+def saludar_con_fecha(request):
+    hoy = datetime.now()
+    saludo = f"hola usuraio, fecha:{hoy.day}/{hoy.month}"
     respuesta_http = HttpResponse(saludo)
     return respuesta_http
