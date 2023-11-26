1fb9118cec0562147d5e9fa6f2ea455dd9cc1a15
Lautaro Reinaga <lautarotreinaga@gmail.com>
2023-11-26 01:00:49 -0300

    ultimo guardado

--- control_estudios/tests.py
+++ control_estudios/tests.py
@@ -2,18 +2,18 @@ from django.test import TestCase
 
 from control_estudios.models import Curso
 # Create your tests here.
-class CursoTest(TestCase)
+class CursoTest(TestCase):
     
     def test_creacion_curso(self):
         curso = Curso(nombre="titulo", comision=100)
         curso.save()
 
-        self.assertEqual(Curso.object.count(), 1)
-        self.assertEqual(curso.nombre, "Titulo")
+        self.assertEqual(Curso.objects.count(), 1)
+        self.assertEqual(curso.nombre, "titulo")
         self.assertEqual(curso.comision, 100)
         
     def test_curso_str(self):
-        curso= Curso(nombre="Terminator", comision=30)
+        curso = Curso(nombre="Terminator", comision=30)
         curso.save()
         
-        self.assertEqual(curso._str_(), "Terminator (30)")
+        self.assertEqual(curso.__str__(), "Terminator (30)")
